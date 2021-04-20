15a7a31469e7613a17fcfedd69f3c28bfcf7072f
Alex López <alex.loplop@educa.jcyl.es>
2021-04-20 19:58:44 +0200

    Creacion clase prueba1

--- actividad4/src/AleatorioEmple.java
+++ actividad4/src/AleatorioEmple.java
@@ -68,3 +68,4 @@ public class AleatorioEmple {
 	}// fin crear
 
 }// fin class
+
---
+++ actividad4/src/prueba1.java
@@ -0,0 +1,6 @@
+
+public class prueba1 {
+	public static void name() {
+		System.out.println("Hola Mundo!");
+	}
+}
